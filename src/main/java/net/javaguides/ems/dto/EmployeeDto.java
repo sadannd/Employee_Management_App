3dbec57707b3a570bcb70505508e0d4cbebaf279
Sada <sadanand.s.kolhe@gmail.com>
2024-02-22 14:06:33 -0600

    first commit

---
+++ src/main/java/net/javaguides/ems/dto/EmployeeDto.java
@@ -0,0 +1,22 @@
+package net.javaguides.ems.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+    private long id;
+    private String firsName;
+    private String lastName;
+    private String email;
+
+}
